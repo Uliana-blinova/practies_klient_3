08647398b12189b93e099b1a7f380190232eda26
Uliana Blinova <ulanablinova0@gmail.com>
2026-03-10 11:37:12 +0700

    add template and methods

--- js/index.js
+++ js/index.js
@@ -23,8 +23,35 @@ Vue.component('kanban-column', {
             required:true
         }
     },
-    template: ``,
+    template: `
+    <div class="column">
+            <h2>{{ title }}</h2>
+            <div class="task-list">
+                <task-card 
+                    v-for="task in tasks" 
+                    :key="task.id" 
+                    :task="task"
+                    :current-status="status"
+                    @move-task="moveTask"
+                    @edit-task="editTask"
+                    @delete-task="deleteTask"
+                ></task-card>
+                <button v-if="status === 'planned'" @click="createTask">+ Добавить задачу</button>
+            </div>
+        </div>
+    `,
     methods: {
-
+        moveTask(payload) {
+            this.$emit('move-task', payload);
+        },
+        editTask(task) {
+            this.$emit('edit-task', task);
+        },
+        deleteTask(id) {
+            this.$emit('delete-task', id);
+        },
+        createTask() {
+            this.$emit('create-task', this.status);
+        }
     }
 })
